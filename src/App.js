6842bd534a1cd804def33d59160c044bccee4370
wulan marsuri <marsuriwulan@gmail.com>
2022-05-27 22:38:43 +0700

    menambahkan file di src

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import Project from './Project';
+import Contact from './Contact';
 
 function App() {
   return (
     <div className="App">
       <h1>'hello this is my first project'</h1>
+      <Home />
+      <About />
+      <Project />
+      <Contact />
     </div>
   );
 }
